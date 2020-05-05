88684a62c3fa008f77db454a51e50d6d48486ee5
Shreyas0896 <shreyas.kotla@gmail.com>
2020-05-04 22:07:14 -0500

    Vanishing Pigs, Key Codes, ASCII values

--- Slingshot.js
+++ Slingshot.js
@@ -14,6 +14,18 @@ class SlingShot{
         World.add(world, this.sling);
     }
 
+    attach(body) {
+
+        this.sling.bodyA = body;
+        
+
+
+
+
+    }
+    
+    
+    
     fly(){
         this.sling.bodyA = null;
     }
